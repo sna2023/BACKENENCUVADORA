94add5d1f812c735ca85e4a7b34050c471bc94b0
encuvadora <snayder2003ch@gmail.com>
2026-06-24 23:29:25 -0500

    fix: resolver conflicto de merge en routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['status' => 'ok', 'app' => config('app.name')]);
 });
-=======
-Route::get('/{any?}', function () {
-    return view('app');
-})->where('any', '.*');
->>>>>>> f19f637fe0187796c64592efa3a3edb8c3f27e5a
