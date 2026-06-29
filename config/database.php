ba105bfc0cefd225479893dee23d1556c5454d1a
encuvadora <snayder2003ch@gmail.com>
2026-06-29 02:12:01 -0500

    fix: disable pgsql prepared statements to fix cached plan error

--- config/database.php
+++ config/database.php
@@ -97,6 +97,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
